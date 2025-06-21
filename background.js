2a297682ef58ff4f6bd7b1113cdd0ab6fddc6233
NavyaMehrotra1 <nmehrot2@jh.edu>
2025-06-21 09:42:44 +0530

    structure for background.js

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed!");
-});
+chrome.alarms.onAlarm.addListener((alarm) => {
+  // to check if something changed on the page 
+
+}); 
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "startTracking") {
+    // create an alarm for this url 
+  }
+}); 
+
